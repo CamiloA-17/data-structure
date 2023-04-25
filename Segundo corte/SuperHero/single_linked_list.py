572ec397510ce8bb69184b5cb368b46fc7526690
Camilo <camiangi1517@gmail.com>
2023-04-24 23:40:38 -0500

    sll completed

--- Segundo corte/SuperHero/single_linked_list.py
+++ Segundo corte/SuperHero/single_linked_list.py
@@ -101,35 +101,36 @@ class SingleLinkedList:
             remove_node.next=None
             self.length -=1   
             
-    def get_node(self, index):
+    def get_node_value(self, index):
         if index < 1 or index > self.length:
-            print('No se encontro el nodo')
-        elif index == 1:
-            return self.head
-        elif index ==self.length:
-            return self.tail
-        else:
-            current_node= self.head
-            node_counter = 1
-            while(index!= node_counter):
-                current_node= current_node.next
-                node_counter += 1
-            return current_node  
-    
-    def get_node_value(self, index): 
-        if index < 1 or index > self.length:
-            print('No se encontro el nodo')
+            print('No se encontro')
         elif index == 1:
             return self.head.value
-        elif index ==self.length:
+        elif index == self.length:
             return self.tail.value
         else:
-            current_node= self.head
+            current_node = self.head
             node_counter = 1
-            while(index!= node_counter):
-                current_node= current_node.next
+            #Validar que el nodo a consultar sea el mismo del contador
+            while(index != node_counter):
+                current_node = current_node.next
                 node_counter += 1
             return current_node.value
+    
+    def get_node(self, index):
+        if index < 1 or index > self.length:
+            return None
+        elif index == 1:
+            return self.head
+        elif index == self.length:
+            return self.tail
+        else:
+            current_node = self.head
+            node_counter = 1
+            while(index != node_counter):
+                current_node = current_node.next
+                node_counter += 1
+            return current_node
             
     def update_node_value(self, index, new_value):
         search_node= self.get_node(index)
@@ -142,31 +143,41 @@ class SingleLinkedList:
             print(" >>No se encontro el nodo<<")
             
     def remove_node(self, index):
-        if index==1:
-            self.shift_node_sll();
-        elif index==self.length:
+        if index == 1:
+            self.shift_node_sll()
+        elif index == self.length:
             self.delete_node_sll_pop()
         else:
-            remove_node= self.get_node(index)
-            if remove_node != None:
-                previous_node= self.get_node(index-1)
-                previous_node.next = remove_node.next
-                remove_node.next= None
+            remove_node_sll = self.get_node(index)
+            if remove_node_sll!= None:
+                previous_node = self.get_node(index - 1)
+                print(self.get_node(index).value)
+                previous_node.next = remove_node_sll.next
+                remove_node_sll.next = None
+                self.length-=1
             else:
-                print( ' >>No se encontro el nodo<< ')
+                print('     >> No se encontro el nodo <<')
                 
-    def revert_node_list(self):
-        if self.length==0:
-            print("No se puede invertir")
-        else:
-            previous_node=None
-            current_node=self.head
-            while current_node != None:
-                following_node= current_node.next
-                current_node.next = previous_node
-                previous_node=current_node
-                current_node=following_node
-            self.head= previous_node
+    def reverse(self):
+        if self.length > 1:
+            aux_head = self.tail
+            aux_tail = self.head
+            if self.length == 2:
+                self.head = aux_head
+                self.head.next = aux_tail
+                self.tail = aux_tail
+                self.tail.next = None
+                return
+            
+            current_node = self.tail
+            for i in range (1, self.length - 1):
+                node = self.get_node(self.length - i)
+                current_node.next = node
+                current_node = node
+            node.next = aux_tail
+            self.head = aux_head
+            self.tail = aux_tail
+            self.tail.next = None
             
     def remove_all_nodes(self):
         self.head.next=None
@@ -174,20 +185,26 @@ class SingleLinkedList:
         self.tail=None
         self.length=0
         
-    def insert_node(self,value,index):
-        new_node = self.Node(value)
-        insert_node=self.get_node(index)
-        if insert_node!= None:
-            if index ==1:
-                new_node.next=self.head
-                self.head=new_node
-                self.length+=1
-            elif index<=self.length:
-                previous_node=self.get_node(index-1)
-                previous_node.next=new_node
-                new_node.next=insert_node
-        else:
-            print("No se encontro el nodo")
+    def insert_node(self, index, value):
+        if self.length !=8:
+            print(f"\n >>>>>> Agregar nodo en la posición {index} <<<<<<")
+            if index == 1:
+                self.create_node_sll_unshift(value)
+            elif index == self.length + 1:
+                self.create_node_sll_ends(value)
+            else:
+                new_node = self.Node(value)
+                actual_node_sll = self.get_node(index)
+                if actual_node_sll != None:
+                    previous_node = self.get_node(index - 1)
+                    print("Se va a mover: ",actual_node_sll.value)
+                    previous_node.next = new_node
+                    new_node.next = actual_node_sll
+                    self.length+=1
+                else:
+                    print(" >>> El índice no es accesible <<< ")
+            return True
+        return False
     
     
                 
--- Segundo corte/SuperHero/window.py
+++ Segundo corte/SuperHero/window.py
@@ -1,4 +1,5 @@
 import pygame, sys
+import webbrowser
 from pygame.locals import *
 from single_linked_list import SingleLinkedList
 from combo_box import ComboBox
@@ -24,24 +25,25 @@ class main:
         self.arbol_image = pygame.image.load("SuperHero/Images/arbol.png")
         self.nodo_image= pygame.image.load("SuperHero/Images/nodo.png")
         self.combo_functions_rect= pygame.Rect(351,185,242,37)
-        self.combo= ComboBox(self.window,['Añadir al inicio','Añadir al final','Eliminar al inicio','Eliminar al final','Invertir la lista', 'Eliminar todos los elementos','Eliminar por posicion','Añadir por posicion','Actualizar valor'],self.combo_functions_rect,self.black_color,'Times New Roman',16,20,self.white_color,self.white_color,40,'MÉTODOS')
+        self.combo= ComboBox(self.window,['Añadir al inicio','Añadir al final','Eliminar al inicio','Eliminar al final','Invertir la lista', 'Eliminar todos los elementos','Eliminar por posicion','Añadir por posicion','Actualizar valor'],self.combo_functions_rect,self.black_color,'Times New Roman',16,20,self.white_color,self.white_color,40,' ')
         self.combo_selected= False
         self.combo_pos_rect= pygame.Rect(862,185,50,37)
-        self.combo_pos= ComboBox(self.window,['1','2','3','4','5','6','7','8'],self.combo_pos_rect,self.black_color,'Times New Roman',16,20,self.white_color,self.white_color,40,'A')
+        self.combo_pos= ComboBox(self.window,['1','2','3','4','5','6','7','8'],self.combo_pos_rect,self.black_color,'Times New Roman',16,20,self.white_color,self.white_color,40,' ')
         self.combo_pos_selected= False
         self.initial_window = True
         self.click_button= False
         self.rect_aux= None
         self.node_aux= None
-        self.bools()
+        self.github_rect= pygame.Rect(850,665,38,36)
+        # self.bools()
         
-    def bools(self):
-        self.spotify_bool= False
-        self.whatsapp_bool= False
-        self.twitter_bool= False
-        self.discord_bool= False
-        self.youtube_bool= False
-        self.appstore_bool= False
+    # def bools(self):
+    #     self.spotify_bool= False
+    #     self.whatsapp_bool= False
+    #     self.twitter_bool= False
+    #     self.discord_bool= False
+    #     self.youtube_bool= False
+    #     self.appstore_bool= False
         
     # def selectedApp(self):
     #     if self.spotify_rect.collidepoint(pygame.mouse.get_pos()):
@@ -77,8 +79,6 @@ class main:
                     self.draw_sll_window()
                     self.combo.draw()
                     self.combo_pos.draw()
-                    # self.select_image()
-                    # self.select_methods()  
             pygame.display.flip()    
             
     def rectGroup(self):
@@ -97,6 +97,7 @@ class main:
         status_text= self.font.render('Estado de la lista: ', True,self.black_color)
         copyright_text= self.font.render('Desarrollado por : Camilo Andres Molano Aristizabal',True,self.black_color)
         copyright_2_text= self.font.render('@ I SEM -2023',True,self.black_color)
+        self.url= "https://github.com/CamiloA-17/data-structure"
         sll_option= self.font.render('SLL',True,self.black_color)
         dll_option= self.font.render('DLL',True,self.black_color)
         pyc_option= self.font.render('PILAS Y COLAS',True,self.black_color)
@@ -117,7 +118,29 @@ class main:
         self.window.blit(self.arbol_image,(822,27))
         self.window.blit(self.options_image,(1082,27))
         
-     
+    def open_github(self):
+        if pygame.mouse.get_pressed()[0]:
+            if self.github_rect.collidepoint(pygame.mouse.get_pos()):
+                webbrowser.open(self.url)
+        
+    def draw_dll_window(self):
+        self.window.fill(self.gray_color)
+        self.top_rect=pygame.draw.rect(self.window,self.white_color,(0,0,1280,87),0,0)
+        self.bottom_rect= pygame.draw.rect(self.window, self.white_color,(0,649,1280,71),0,0)
+        self.list_rect= pygame.draw.rect(self.window,self.white_color,(154,460,1020,159),0,20)
+        self.spotify_rect_initial = pygame.Rect(418,271,114,135)
+        self.whatsapp_rect_initial = pygame.Rect(608,271,114,135)
+        self.twitter_rect_initial = pygame.Rect(798,271,114,135)  
+        self.text()
+        init_text= self.font.render('PARA INICIAR DEBES SELECCINAR UNA IMAGEN QUE SERA LA CABEZA DE LA LISTA',True, self.black_color)
+        self.window.blit(init_text,(341,220))
+        self.window.blit(self.spotify_image, (418,271))
+        self.window.blit(self.whatsapp_image,(608,271))
+        self.window.blit(self.twitter_image,(798,271))
+        self.window.blit(self.uam_image,(1188,658))
+        self.window.blit(self.github_image,(850,665))
+        self.open_github()        
+    
     def draw_sll_window(self):
         self.window.fill(self.gray_color)
         self.top_rect=pygame.draw.rect(self.window,self.white_color,(0,0,1280,87),0,0)
@@ -143,26 +166,27 @@ class main:
         self.window.blit(self.github_image,(850,665)) 
         self.rectGroup()
         self.visual_list() 
-        self.select_image()          
+        self.select_image()
+        self.open_github()          
     
     def initial(self):
-            self.window.fill(self.gray_color)
-            self.top_rect=pygame.draw.rect(self.window,self.white_color,(0,0,1280,87),0,0)
-            self.bottom_rect= pygame.draw.rect(self.window, self.white_color,(0,649,1280,71),0,0)
-            self.list_rect= pygame.draw.rect(self.window,self.white_color,(154,460,1020,159),0,20)
-            self.spotify_rect_initial = pygame.Rect(418,271,114,135)
-            self.whatsapp_rect_initial = pygame.Rect(608,271,114,135)
-            self.twitter_rect_initial = pygame.Rect(798,271,114,135)  
-            self.text()
-            init_text= self.font.render('PARA INICIAR DEBES SELECCINAR UNA IMAGEN QUE SERA LA CABEZA DE LA LISTA',True, self.black_color)
-            self.window.blit(init_text,(341,220))
-            self.window.blit(self.spotify_image, (418,271))
-            self.window.blit(self.whatsapp_image,(608,271))
-            self.window.blit(self.twitter_image,(798,271))
-            self.window.blit(self.uam_image,(1188,658))
-            self.window.blit(self.github_image,(850,665))
-            
-            
+        self.window.fill(self.gray_color)
+        self.top_rect=pygame.draw.rect(self.window,self.white_color,(0,0,1280,87),0,0)
+        self.bottom_rect= pygame.draw.rect(self.window, self.white_color,(0,649,1280,71),0,0)
+        self.list_rect= pygame.draw.rect(self.window,self.white_color,(154,460,1020,159),0,20)
+        self.spotify_rect_initial = pygame.Rect(418,271,114,135)
+        self.whatsapp_rect_initial = pygame.Rect(608,271,114,135)
+        self.twitter_rect_initial = pygame.Rect(798,271,114,135)  
+        self.text()
+        init_text= self.font.render('PARA INICIAR DEBES SELECCINAR UNA IMAGEN QUE SERA LA CABEZA DE LA LISTA',True, self.black_color)
+        self.window.blit(init_text,(341,220))
+        self.window.blit(self.spotify_image, (418,271))
+        self.window.blit(self.whatsapp_image,(608,271))
+        self.window.blit(self.twitter_image,(798,271))
+        self.window.blit(self.uam_image,(1188,658))
+        self.window.blit(self.github_image,(850,665))
+        self.open_github()          
+    
     def click_on_head(self):
         mouse_pos= pygame.mouse.get_pos()
         if pygame.mouse.get_pressed()[0]:
@@ -178,7 +202,7 @@ class main:
                 inst.create_node_sll_unshift('Twitter')
                 inst.show_list()
                 self.initial_window = False
-                
+
     def visual_list(self):
         gap=0
         for i in range(1,inst.length+1):
@@ -195,8 +219,7 @@ class main:
             elif inst.get_node_value(i) == 'App Store':
                 self.window.blit(self.appstore_image,(164+gap,472))    
             gap+=130
-            
-                
+
     # def clickOnButton(self):
     #     self.methodsSelection()
     #     if self.button_rect.collidepoint(pygame.mouse.get_pos()):
@@ -207,16 +230,15 @@ class main:
     #     if not pygame.mouse.get_pressed()[0]:
     #         self.click_button = False
     #         return self.click_button
-     
+
     # def methodsSelection(self):
     #     if self.spotify_bool or self.whatsapp_bool or self.twitter_bool or self.discord_bool or self.youtube_bool or self.appstore_bool:
     #         if self.combo.getIndex() == '1':
     #             inst.create_node_sll_unshift(self.selectedApp())
     #             inst.show_list()
-       
-    
+
     def select_image(self):
-        self.click_item= bool
+        # self.click_item= bool
         if pygame.mouse.get_pressed()[0]:
             if self.whatsapp_rect.collidepoint(pygame.mouse.get_pos()):
                 self.node_aux= 'Whatsapp'
@@ -257,10 +279,9 @@ class main:
                     self.node_aux=None
                     inst.show_list()
                 if self.combo.getIndex()== 5:
-                    inst.revert_node_list()
+                    inst.reverse()
                     self.node_aux=None
                     inst.show_list()
-                    # No funciona correctamente
                 if self.combo.getIndex()==6:
                     inst.remove_all_nodes()
                     self.node_aux=None
@@ -269,33 +290,13 @@ class main:
                     inst.remove_node(self.combo_pos.getIndex())
                     self.node_aux=None
                     inst.show_list()
-                    # No funciona correctamente
                 if self.combo.getIndex()==8:
                     if self.node_aux is not None:
-                        inst.insert_node(self.node_aux, self.combo_pos.getIndex())
+                        inst.insert_node(self.combo_pos.getIndex(),self.node_aux)
                         self.node_aux=None
                         inst.show_list()
-                        # No funciona correctamente
                 if self.combo.getIndex()==9:
                     if self.node_aux is not None:
                         inst.update_node_value(self.combo_pos.getIndex(),self.node_aux)
                         self.node_aux=None
                         inst.show_list()
-                
-
-                    
-                        
-                    
-                
-                            
-                    
-                    
-                    
-        
-    
-        
-                    
-        
-        
-        
-    
